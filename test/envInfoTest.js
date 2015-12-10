b209743fd56cb76fafeac16f2ec80bea9ed688d2
Lu Rongkai <lurongkai@gmail.com>
2015-12-10 17:43:00 +0800

    test fx.

---
+++ test/envInfoTest.js
@@ -0,0 +1,11 @@
+
+var assert = require('assert');
+
+describe("hello", function() {
+	describe("world", function() {
+		it("should return true", function() {
+			assert.equal(1, 1);
+			assert.equal("hh", "hh");
+		});
+	});
+});
